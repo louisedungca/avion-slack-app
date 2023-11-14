682fbc512e45c4b193a2e8cf05e501b78effc0cf
Louise Dungca <dungca.louisechristine@gmail.com>
2023-11-14 16:28:41 +0800

    feat: add specific error status for error page

--- src/pages/general/ErrorPage.jsx
+++ src/pages/general/ErrorPage.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
-import { Link, useNavigate, useRouteError } from 'react-router-dom';
+import { Link, isRouteErrorResponse, useNavigate, useRouteError } from 'react-router-dom';
 import { ArrowUturnLeftIcon, HomeIcon } from '@heroicons/react/24/solid';
-
 
 const Error = () => {
   const error = useRouteError();
   const navigate = useNavigate();
+  
+  let errorMessage = '';
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      errorMessage = "This page doesn't exist.";
+    } else if (error.status === 401) {
+      errorMessage = "You aren't allowed on this page.";
+    } else if (error.status === 503) {
+      errorMessage = "Looks like our system is down.";
+    } else if (error.status === 418) {
+      errorMessage = "🫖";
+    }
+  } else {
+    errorMessage = error.message || error.statusText || "Something went wrong";
+  }
 
   return (
     <div className='error-page'>
-      <h1>Hmm. It seems like we've got a little problem.</h1>
-      <p>{error.message || error.statusText}</p>
-      <div>
+      <h1>Hmm, there is quite a problem!</h1>
+      <h2>{errorMessage}</h2>
+
+      <div className='btns-wrapper'>
         <button 
           onClick={() => {navigate(-1)}}
         >
           <ArrowUturnLeftIcon width={20}/>
-          <span>Go back</span>
         </button>
-        <Link
-          to="/"
+
+        <button 
+          onClick={() => {navigate('/')}}
         >
-          <HomeIcon width={20} />
-          <span>Go home</span>
-        </Link>
+          <HomeIcon width={20}/>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
