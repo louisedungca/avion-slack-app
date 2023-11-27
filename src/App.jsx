24b0885c39d659705e4d5f865100fd6972a77d11
cyrilcero <cyril.cero@gmail.com>
2023-11-27 23:35:45 +0800

    revert: to old commit state

--- src/App.jsx
+++ src/App.jsx
@@ -14,77 +14,81 @@ function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <l.WelcomeLayout />,
+      element: <p.WelcomePage />,
       errorElement: <p.ErrorPage />,
       loader: u.isLoggedInLoader,
+    },
+    {
+      path: 'signup',
+      element: <p.SignUpPage />,
+      errorElement: <p.ErrorPage />,
+      loader: u.isLoggedInLoader,
+    },
+    {
+      path: 'login',
+      element: <p.LoginPage />,
+      errorElement: <p.ErrorPage />,
+      loader: u.isLoggedInLoader,
+    },
+    {
+      path: 'c',
+      element: <l.MainPage />,
+      errorElement: <p.ErrorPage />,
+      loader: u.accessDashLoader,
       children: [
         {
-          path: 'signup',
-          element: <p.SignUpPage />,
-          errorElement: <p.ErrorPage />,
-          loader: u.isLoggedInLoader,
-        },
-        {
-          path: 'login',
-          element: <p.LoginPage />,
-          errorElement: <p.ErrorPage />,
-          loader: u.isLoggedInLoader,
-        },
-        {
-          path: 'c',
-          element: <l.MainPage />,
+          path: 'chats',
+          element: <l.ChatLayout />,
           errorElement: <p.ErrorPage />,
           loader: u.accessDashLoader,
           children: [
             {
-              path: 'chats',
-              element: <l.ChatLayout />,
+              index: true,
+              element: <c.Placeholder type={'chat'} />,
               errorElement: <p.ErrorPage />,
               loader: u.accessDashLoader,
-              children: [
-                {
-                  index: true,
-                  element: <c.Placeholder type={'chat'} />,
-                  errorElement: <p.ErrorPage />,
-                  loader: u.accessDashLoader,
-                },
-                {
-                  path: ':id',
-                  element: <p.ChatBox />,
-                  errorElement: <p.ErrorPage />,
-                  loader: u.accessDashLoader,
-                }
-              ],
             },
             {
-              path: 'channels',
-              element: <l.ChannelLayout />,
+              path: ':id',
+              element: <p.ChatBox />,
               errorElement: <p.ErrorPage />,
               loader: u.accessDashLoader,
-              children: [
-                {
-                  index: true,
-                  element: <c.Placeholder type={'channel'} />,
-                  errorElement: <p.ErrorPage />,
-                  loader: u.accessDashLoader,
-                },
-                {
-                  path: ':channel_id',
-                  element: <p.ChannelBox />,
-                  errorElement: <p.ErrorPage />,
-                  loader: u.accessDashLoader,
-                }
-              ],
-            },
-            {
-              path: 'people',
-              element: <p.People />,
-              errorElement: <p.ErrorPage />,
-              loader: u.accessDashLoader,
-            },
+            }
           ],
         },
-      ]
+        {
+          path: 'channels',
+          element: <l.ChannelLayout />,
+          errorElement: <p.ErrorPage />,
+          loader: u.accessDashLoader,
+          children: [
+            {
+              index: true,
+              element: <c.Placeholder type={'channel'} />,
+              errorElement: <p.ErrorPage />,
+              loader: u.accessDashLoader,
+            },
+            {
+              path: ':channel_id',
+              element: <p.ChannelBox />,
+              errorElement: <p.ErrorPage />,
+              loader: u.accessDashLoader,
+            }
+          ],
+        },
+        {
+          path: 'people',
+          element: <p.People />,
+          errorElement: <p.ErrorPage />,
+          loader: u.accessDashLoader,
+        },
+        // {
+        //   path: 'profile',
+        //   element: <p.Profile />,
+        //   errorElement: <p.ErrorPage />,
+        //   loader: u.accessDashLoader,
+        // },
+      ],
     },
   ]);
 
--- src/components/navbar/WelcomeNavbar.jsx
+++ src/components/navbar/WelcomeNavbar.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
 import smileylogo from '../../assets/smileylogo.png';
-import { useNavigate } from 'react-router-dom';
 
 function WelcomeNavbar() {
-  const navigate = useNavigate();
   return (
     <nav className='welcomenav'>
-      <div className="logonav" onClick={() => {navigate('/')}}>
+      <div className="logonav">
         <img src={smileylogo} alt="slackify-logo" className='logo-img'/>
         <h2 className='brand-title'>Slackify</h2>
       </div>
 
       <div className="welcomenav-btns">
-        <button 
-          className='welcome-btn outline'
-          onClick={() => {navigate('/signup')}}
-        >
+        <button className='welcome-btn outline'>
           Create Account
         </button>
 
-        <button 
-          className='welcome-btn'
-          onClick={() => {navigate('/login')}}
-        >
+        <button className='welcome-btn'>
           Login
         </button>
       </div>
