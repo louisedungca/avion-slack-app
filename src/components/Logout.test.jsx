0446573f0a82199905794793e86b774213a01cb0
Louise Dungca <dungca.louisechristine@gmail.com>
2023-11-29 21:53:51 +0800

    feat: add more test on logout

--- src/components/Logout.test.jsx
+++ src/components/Logout.test.jsx
@@ -1,26 +1,52 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it } from 'vitest';
 import { MemoryRouter } from 'react-router-dom';
 import Logout from "./Logout";
 
+const uid = 'email@email.com'; 
+const mockUserData = { uid };
+const mockLocalStorage = {
+  getItem: key => JSON.stringify(key === 'UserData' ? mockUserData : null),
+  removeItem: key => { delete global.localStorage[key]; },
+};
+
 describe('Logout', () => {
   it('Renders email from local storage', () => {
-    const email = 'email@email.com';
-    const mockLocalStorage = {
-      getItem: () => JSON.stringify({ uid: email }),
-    }
-
-    globalThis.localStorage = mockLocalStorage;
+    global.localStorage = mockLocalStorage;
+    const mockOnClose = vi.fn();
 
     render(
       <MemoryRouter>
-        <Logout />
+        <Logout onClose={mockOnClose} />
       </MemoryRouter>
     );
 
     screen.debug();
-    expect(screen.getByText(`We'll sign you out as ${email}`, { exact: false })).toBeTruthy();
+    expect(screen.getByText(`We'll sign you out as ${uid}`, { exact: false })).toBeTruthy();
 
     global.localStorage = undefined;
-  })
+  });
+
+  it('Renders to / on click of Sign out button', async () => {
+    global.localStorage = mockLocalStorage;
+    const mockOnClose = vi.fn();
+
+    render(
+      <MemoryRouter>
+        <Logout onClose={mockOnClose} />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByRole('button', 
+      { name: 'Sign out' }, 
+      { exact: false },
+    ));
+    await waitFor(() => {
+      expect(window.location.pathname).toBe('/');
+    });
+
+    global.localStorage = undefined;
+  });
 });
+
+
